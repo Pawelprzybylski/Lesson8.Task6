0362dd0f7756972e0c71278a2f955d734c8f6fff
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-06-04 17:08:00 +0200

    commit

---
+++ src/main/java/BillCalculator.java
@@ -0,0 +1,11 @@
+public class BillCalculator {
+    public static double calculate(double billValue,float paymentForService){
+        return billValue+paymentForService;
+    }
+    public static double calculate(double billValue,float paymentForService,double discount){
+        return  (billValue*discount)+paymentForService;
+    }
+    public static double calculate(double billValue, float paymentForService, short takeoutPack){
+        return billValue+paymentForService+takeoutPack;
+    }
+}
